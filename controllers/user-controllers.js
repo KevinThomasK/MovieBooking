75ce89a43f849339e694c3bd3c11122667edfdfd
Kevin Thomas <kevinthomas0420@gmail.com>
2023-09-16 16:40:57 +0530

    update and delete functionality added

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const getAllUsers = async (req, res, next) => {
   let users;
@@ -28,9 +29,11 @@ export const signup = async (req, res, next) => {
     return res.status(422).json({ message: "Invalid input data" });
   }
 
+  const hashedPassword = bcrypt.hashSync(password);
+
   let user;
   try {
-    user = new User({ name, email, password });
+    user = new User({ name, email, password: hashedPassword });
     user = await user.save();
   } catch (error) {
     return console.log(error);
@@ -40,4 +43,51 @@ export const signup = async (req, res, next) => {
     res.status(500).json({ message: "Unexpected error" });
   }
   return res.status(201).json({ user });
+};
+
+export const updateUser = async (req, res, next) => {
+  const id = req.params.id;
+
+  const { name, email, password } = req.body;
+  if (
+    !name &&
+    name.trim() === "" &&
+    !email &&
+    email.trim() === "" &&
+    !password &&
+    password.trim() === ""
+  ) {
+    return res.status(422).json({ message: "Invalid input data" });
+  }
+
+  const hashedPassword = bcrypt.hashSync(password);
+
+  let user;
+  try {
+    user = await User.findByIdAndUpdate(id, {
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!user) {
+    return res.status(500).json({ message: "something went wrong" });
+  }
+  res.status(200).json({ message: "updated successfully" });
+};
+
+export const deleteUser = async (req, res, next) => {
+  const id = req.params.id;
+  let user;
+  try {
+    user = await User.findByIdAndRemove(id);
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!user) {
+    return res.status(500).json({ message: "something went wrong" });
+  }
+  res.status(200).json({ message: "Deleted successfully" });
 };
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getAllUsers, signup } from "../controllers/user-controllers.js";
+import {
+  deleteUser,
+  getAllUsers,
+  signup,
+  updateUser,
+} from "../controllers/user-controllers.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getAllUsers);
-userRouter.post("/signup",signup );
+userRouter.post("/signup", signup);
+userRouter.put("/:id", updateUser);
+userRouter.delete("/:id", deleteUser);
 
 export default userRouter;
