78fef21eef8a0716fdb3c88a6279d036ac8d1ba8
Kevin Thomas <kevinthomas0420@gmail.com>
2023-09-17 19:48:48 +0530

    add user-booking relation ,add booking post delete api

--- controllers/booking-controller.js
+++ controllers/booking-controller.js
@@ -1,7 +1,28 @@
+import mongoose from "mongoose";
 import Bookings from "../models/Bookings.js";
+import Movie from "../models/Movie.js";
+import User from "../models/User.js";
 
 export const newBooking = async (req, res, next) => {
   const { movie, date, seatNumber, user } = req.body;
+
+  let existingMovie;
+  let existingUser;
+
+  try {
+    existingMovie = await Movie.findById(movie);
+    existingUser = await User.findById(user);
+  } catch (error) {
+    return console.log(error);
+  }
+
+  if (!existingMovie) {
+    return res.status(404).json({ message: "movie not found" });
+  }
+
+  if (!existingUser) {
+    return res.status(404).json({ message: "User not found" });
+  }
 
   let booking;
 
@@ -13,7 +34,16 @@ export const newBooking = async (req, res, next) => {
       user,
     });
 
-    booking = await booking.save();
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    existingUser.bookings.push(booking);
+    existingMovie.bookings.push(booking);
+    await existingUser.save({ session });
+    await existingMovie.save({ session });
+    await booking.save({ session });
+    session.commitTransaction();
+
+    
   } catch (error) {
     return console.log(error);
   }
@@ -24,3 +54,46 @@ export const newBooking = async (req, res, next) => {
 
   return res.status(200).json({ booking });
 };
+
+
+export const getBookingById = async (req,res,next) => {
+  const id = req.params.id;
+  let booking;
+  try {
+    booking = await Bookings.findById(id);
+
+
+  } catch (error) {
+    return console.log(error)
+  }
+  if(!booking){
+    return res.status(500).json({ message: "unexpected error" });
+  }
+  return res.status(200).json({ booking });
+}
+
+
+export const deleteBooking = async (req,res,next) => {
+  const id = req.params.id;
+  let booking;
+  try {
+    booking = await Bookings.findByIdAndRemove(id).populate("user movie");
+    // console.log(booking);
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await booking.user.bookings.pull(booking);
+    await booking.movie.bookings.pull(booking);
+
+    await booking.movie.save({session});
+    await booking.user.save({session});
+    session.commitTransaction();
+
+  } catch (error) {
+    return console.log(error)
+  }
+  if(!booking){
+    return res.status(500).json({ message: "unable to delete" });
+  }
+  return res.status(200).json({ message:"Succesfully deleted" });
+}
+
